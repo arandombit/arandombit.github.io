74bf14b1953a21b7c575dda22cf427249a7e332d
arandombit <a.random.luis@proton.me>
2024-09-11 12:25:41 -0400

    remove boilerplate-related files

--- app.js
+++ app.js
@@ -1,20 +1,16 @@
 // @ts-check
-const { defineConfig, createNotesQuery } = require("./.app/app-config");
+const { defineConfig, createNotesQuery } = require('./.app/app-config');
 
 module.exports = defineConfig({
-  title: "Eleventy Notes",
-  description:
-    "A simple, lightweight, and flexible note-taking template for Eleventy.",
-  editThisNote: {
-    url: "https://github.com/rothsandro/eleventy-notes/edit/{{branch}}/{{file}}",
-  },
+  title: 'A Random Bit',
+  description: 'Some random thoughts',
   customProperties: {
     properties: [
       {
-        path: "props",
+        path: 'props',
         options: {
           date: {
-            locale: "en-US",
+            locale: 'en-US',
           },
         },
       },
@@ -23,81 +19,27 @@ module.exports = defineConfig({
   sidebar: {
     links: [
       {
-        url: "https://github.com/rothsandro/eleventy-notes",
-        label: "GitHub / Support",
-        icon: "github",
-      },
-      {
-        url: "https://www.buymeacoffee.com/sandroroth",
-        label: "Buy me a coffee",
-        icon: "coffee",
+        url: 'https://github.com/arandombit',
+        label: 'GitHub',
+        icon: 'github',
       },
     ],
     sections: [
       {
-        label: "Introduction",
+        label: 'Posts',
         groups: [
           {
             query: createNotesQuery({
-              pattern: "^/[^/]+$",
-            }),
-          },
-        ],
-      },
-      {
-        label: "Guides",
-        groups: [
-          {
-            label: "Writing Notes",
-            query: createNotesQuery({
-              pattern: "^/Writing/",
-              tree: {
-                replace: {
-                  "^/\\w+": "",
-                },
-              },
-            }),
-          },
-          {
-            label: "Organizing Notes",
-            query: createNotesQuery({
-              pattern: "^/Organizing/",
-            }),
-          },
-          {
-            label: "Core Features",
-            query: createNotesQuery({
-              pattern: "^/Features/",
-              tree: {
-                replace: {
-                  "^/\\w+": "",
-                },
-              },
-            }),
-          },
-          {
-            label: "Deployment",
-            query: createNotesQuery({
-              pattern: "^/Deployment/",
-            }),
-          },
-        ],
-      },
-      {
-        label: "Releases",
-        groups: [
-          {
-            query: createNotesQuery({
-              pattern: "^/Releases/",
-            }),
-          },
-        ],
+              pattern: '^/Post/'
+            })
+          }
+        ]
       },
     ],
   },
   tags: {
     map: {
-      "dynamic-content": "dynamic content",
+      'dynamic-content': 'dynamic content',
     },
   },
 });
